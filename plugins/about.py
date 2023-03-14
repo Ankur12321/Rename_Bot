ba15015eab6b2c196cc9574cf811c032ba452bfa
Ankur12321 <84258110+Ankur12321@users.noreply.github.com>
2023-03-14 13:51:56 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -12,4 +12,4 @@ async def start(client,message):
 	data = find_one(int(botid))
 	total_rename = data["total_rename"]
 	total_size = data["total_size"]
-	await message.reply_text(f"Origional BOT :- <a href='https://t.me/bamel_file_renamer_bot'>Bamel File Renamer</a>\nCreater :- <a href='https://t.me/Bamel_Shab'>🦋LazyDeveloper🦋</a>\nLanguage :- Python3\nLibrary :- Pyrogram 2.0\nServer :- KOYEB\nTotal Renamed File :- {total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))} \n\n Thank you **<a href='https://t.me/Bamel_Shab'>Bamel_Shabr</a>** for your hard work \n\n❤️ we love you <a href='https://t.me/Bamel_Shab'>**Bamel_Shab**</a> ❤️",quote=True)
+	await message.reply_text(f"Origional BOT :- <a href='https://t.me/bamel_file_renamer_bot'>Bamel File Renamer</a>\nCreater :- <a href='https://t.me/Bamel_Shab'>🦋Bamel Shab🦋</a>\nLanguage :- Python3\nLibrary :- Pyrogram 2.0\nServer :- KOYEB\nTotal Renamed File :- {total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))} \n\n Thank you **<a href='https://t.me/Bamel_Shab'>Bamel Shab</a>** for your hard work \n\n❤️ we love you <a href='https://t.me/Bamel_Shab'>**Bamel Shab**</a> ❤️",quote=True)
